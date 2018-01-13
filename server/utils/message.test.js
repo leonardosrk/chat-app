cfdd3309d274e68e4c99213038f14d1cbf3e7ebc
Leonardo Nelson <leonardosn9@gmail.com>
2018-01-13 21:48:30 +0100

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,14 @@
+const expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate the correct message object', () => {
+    var from = 'Leonardo';
+    var text = 'Hello there!';
+    var message = generateMessage(from, text);
+    expect(message.from).toBe(from);
+    expect(message.text).toBe(text);
+    expect(typeof message.createdAt).toBe('number');
+  });
+});
